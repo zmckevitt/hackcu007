b9caac790a26a7ad5f3ef878e0d1de3304eda013
Zack <zack.mckevitt@gmail.com>
2021-03-06 13:30:26 -0700

    new flask server

--- json_io.py
+++ json_io.py
@@ -1,10 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+#!flask/bin/python
+import sys
+from flask import Flask, render_template, request, redirect, Response
+import random, json
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def output():
-	#return render_template("index.html", name="Joe")
-    return "Hello World!"
+	return render_template("index.html", name="Joe")
+    #return "Hello World!"
+
+@app.route('/receiver', methods = ['GET','POST'])
+def worker():
+	# read json + reply
+	data = request.get_json()
+	result = 'peen'
+	for item in data:
+    #     pass
+
+	# 	# loop over every row
+	# 	result += str(item['make']) + '\n'
+	return result
 
 if __name__ == "__main__":
-	app.run()
+    # run!
+	app.run("0.0.0.0", "5020")
