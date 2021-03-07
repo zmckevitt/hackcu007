a889d2c84b8ec4db65138815614d17de01e42a72
Trevor Peterson <trevor.peterson@colorado.edu>
2021-03-07 03:00:59 -0700

    Updated the pydata file to include top 3 genres and other

--- spotify_data.py
+++ spotify_data.py
@@ -1,44 +1,17 @@
-from __future__ import print_function
-import sys
 import spotipy
-import os
 import spotipy.util as util
-from spotipy.oauth2 import SpotifyClientCredentials
-
-if len(sys.argv) > 2:
-    username = sys.argv[1]
-    time = sys.argv[2]
-else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-
-os.environ["SPOTIPY_CLIENT_ID"] = "7e9f9c2e77b64e66a6c20b5b3eb7b479"
-os.environ["SPOTIPY_CLIENT_SECRET"] = "c4bb8e27f686455182b644fb10b61d6e"
-os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:7777/callback"
-
 def func(user,time_range):
-    #username='31exjzvo2nlf4xdgeffjq3ndafwq'
+    username='31exjzvo2nlf4xdgeffjq3ndafwq'
     client_id='7e9f9c2e77b64e66a6c20b5b3eb7b479'
     client_secret='c4bb8e27f686455182b644fb10b61d6e'
     ret=[]
     redirect_uri = 'http://localhost:7777/callback'
     scope = 'user-top-read'
     token = util.prompt_for_user_token(username=username, 
-                                   scope=scope,
+                                   scope=scope, 
                                    client_id=client_id,   
                                    client_secret=client_secret,     
-                                   redirect_uri=redirect_uri
-                                   )
-
-    #print("HERE")
-
-    if(time == 'week'):
-        time_range = 'short_term'
-    if(time == 'month'):
-        time_range = 'medium_term'
-    if(time == 'year'):
-        time_range = 'long_term'
-
+                                   redirect_uri=redirect_uri)
     sp = spotipy.Spotify(auth=token)
     
     top_tracks = sp.current_user_top_tracks(limit=20, offset=0, time_range=time_range)
@@ -60,7 +33,6 @@ def func(user,time_range):
         ret.append((song_name,artist_name,time_ms,genres,ret_features))
     top_G=[]
     num=[]
-    
     for i in range(len(ret)):
         for j in range(len(ret[i][3])) :
             if(ret[i][3][j] in top_G):
@@ -68,13 +40,31 @@ def func(user,time_range):
             else:
                 top_G.append(ret[i][3][j])
                 num.append(1)
+    file1 = open("poop.txt","w")
+    newline = '\n'
     max_value = max(num)
     max_index = num.index(max_value)
-#     print(top_G[max_index])
-    file1 = open("txt_files/out.txt","w") 
+    file1.write(top_G[max_index])
+    file1.write(newline)
+    num.pop(max_index)
+    top_G.pop(max_index)
+    max_value_2 = max(num)
+    max_index = num.index(max_value_2)
+    file1.write(top_G[max_index])
+    file1.write(newline)
+    num.pop(max_index)
+    top_G.pop(max_index)
+    max_value_3 = max(num)
+    max_index = num.index(max_value_3)
+    file1.write(top_G[max_index])
+    file1.write(newline)
+    num.pop(max_index)
+    top_G.pop(max_index)
+     
     topS=[]
     topA=[]
     topD=[]
+    topE=[]
     for i in ret:
         for j in range(len(i)):
             if(j==0):
@@ -82,14 +72,41 @@ def func(user,time_range):
             if(j==1):
                 topA.append(i[j])
     len1=len(ret)
-    temp=[]                
+    temp=[]
+    
+
     for i in range(0,len1-1): 
         for j in range(0, len1-1): 
             if ret[j][4][0][1] < ret[j+1][4][0][1] : 
                 ret[j], ret[j+1] = ret[j+1], ret[j]
     for i in range(0,10):
         topD.append(ret[i][0])
-    
+
+    for i in range(0,len1-1): 
+        for j in range(0, len1-1): 
+            if ret[j][4][1][1] < ret[j+1][4][1][1] : 
+                ret[j], ret[j+1] = ret[j+1], ret[j]
+    for i in range(0,10):
+        topE.append(ret[i][0])
+
+
+    tot_valence = 0
+    avg_valence = 0
+    # finding how sad the person is
+    for i in range (0,len1-1):
+        tot_valence += ret[i][4][9][1]
+    avg_valence = tot_valence/len1
+    sad_string = ""
+    if(avg_valence < 0.25):
+        sad_string = "... see a therapist." + '\n'
+    elif(avg_valence < 0.5):
+        sad_string = "Your music's on the sadder side, but we're sure you'll be fine" + '\n'
+    elif(avg_valence < 0.75):
+        sad_string = "Your music seems on the happier side, we'd be on the happier side if you venmoed ;)" + '\n'
+    elif(avg_valence < 1):
+        sad_string = "Your music is a little too happy. Might want to settle down a little" + '\n'
+    file1.write(sad_string)
+
     for i in range(0,10):
         temp=str(topS[i])+'\n'
         file1.write(temp)
@@ -99,9 +116,8 @@ def func(user,time_range):
     for i in range(0,10):
         temp=str(topD[i])+'\n'
         file1.write(temp)
-    
-    file1.write(top_G[max_index])
+    for i in range(0,10):
+        temp=str(topE[i])+'\n'
+        file1.write(temp)
     return ret
-
-
-func('a',time)
+func('a','medium_term')
